058e7987490ebe06bfd627132950273bf4e1f635
Christopher Wang <wang967@usc.edu>
2018-10-30 21:29:05 -0700

    complete starter code

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Icon } from 'react-native-elements';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
@@ -6,40 +6,43 @@ import LoginScreen from './screens/LoginScreen';
 import WeatherScreen from './screens/WeatherScreen';
 import ToDoList from './screens/ToDoList';
 
-export default createStackNavigator(
-    {
-        LoginScreen: LoginScreen,
-        MainScreen: createBottomTabNavigator({
-            WeatherScreen: {
-                screen: WeatherScreen,
-                navigationOptions: {
-                    tabBarIcon: () => (
-                        <Icon
-                            name='sun'
-                            type='feather'
-                            color='#517fa4'
-                            size={24}
-                        />
-                    ),
-                }
-            },
-            ToDoList: {
-                screen: ToDoList,
-                navigationOptions: {
-                    tabBarIcon: () => (
-                        <Icon
-                            name='ios-list'
-                            type='ionicon'
-                            color='#000000'
-                            size={40}
-                            style={{ marginBottom: 10 }}
-                        />
-                    ),
-                }
-            },
-        }),
-    },
-    {
-        initialRouteName: 'LoginScreen',
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                <Text style={styles.instructions}>To get started, edit App.js</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
+            </View>
+        );
     }
-);
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
